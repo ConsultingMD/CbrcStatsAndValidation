aeba08de77f4f0c5af44d6a208166decf53aa2d9
Zach Lee <zach.lee@grandrounds.com>
2019-03-08 16:21:46 -0800

    inital stats and validation

---
+++ cbrcStats/visitsPerLocationFreqDist.sql
@@ -0,0 +1,16 @@
+-- frequency (in log base 10) of counts per location
+SELECT Count(b.count_of_count_per_npi) as cc,
+       count_of_count_per_npi
+FROM
+(
+  SELECT cast(log(number_of_visits) AS int) count_of_count_per_npi
+
+  FROM (
+         SELECT normalized_npi,
+                COUNT(gr_pid) number_of_visits
+         FROM claims_spectrum.main_raw_cbrc_bronze_20190307203540
+         GROUP BY normalized_npi
+       ) AS base
+) as b
+GROUP BY count_of_count_per_npi
+ORDER BY count_of_count_per_npi;
